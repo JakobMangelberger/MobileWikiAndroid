d05be74e11ac292f2e4ad05b820615b5a83f16c8
Irfan Sehic <Irfan Sehic@Irfan-Thinkpad>
2013-04-24 12:30:45 +0200

    [1, 2] Added a preliminary version of the app. No functionality, just an actionbar with two options and a hello world text (IS, HB)

--- MobileWiki/src/com/mobilewiki/MainActivity.java
+++ MobileWiki/src/com/mobilewiki/MainActivity.java
@@ -1,21 +1,37 @@
 package com.mobilewiki;
 
 import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
+		//Remove title bar
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+	} 
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		
+		// Now we add some items to the action bar
+		MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main, menu);
+
+		
+		
+//		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
