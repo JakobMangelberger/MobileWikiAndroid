feb282db6e94bc4c171b5db19536137e6e72e40f
Stefan Painhapp <stefan.painhapp@gmail.com>
2013-05-29 14:17:34 +0200

    [test4, test5] Added tests for PreviewActivity und EditorActivity (SP, PO)

---
+++ wikitest/src/com/mobilewiki/test/MobileWikiTestEditorActivity.java
@@ -0,0 +1,31 @@
+package com.mobilewiki.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.ListView;
+
+import com.mobilewiki.R;
+import com.mobilewiki.SearchActivity;
+
+public class MobileWikiTestEditorActivity extends
+ActivityInstrumentationTestCase2<SearchActivity>{
+
+	@SuppressWarnings("deprecation")
+	public MobileWikiTestEditorActivity() {
+		super("com.mobilewiki", SearchActivity.class);
+	}
+	
+	private ListView listView;
+	private SearchActivity activity;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+
+	}
+
+	public void test_view() {
+		activity = getActivity();
+		listView = (ListView) activity.findViewById(R.id.listView1);
+
+		assertNotNull(listView);
+	}
+}
--- wikitest/src/com/mobilewiki/test/MobileWikiTestMainActivity.java
+++ wikitest/src/com/mobilewiki/test/MobileWikiTestMainActivity.java
@@ -15,17 +15,16 @@ import android.widget.TextView;
 public class MobileWikiTestMainActivity extends
 		ActivityInstrumentationTestCase2<MainActivity> {
 
-	private TextView field;
-	private MainActivity activity;
-
 	@SuppressWarnings("deprecation")
 	public MobileWikiTestMainActivity() {
 		super("com.mobilewiki", MainActivity.class);
 	}
 
+	private TextView field;
+	private MainActivity activity;
+
 	protected void setUp() throws Exception {
 		super.setUp();
-
 	}
 
 	public void test_view() {
@@ -36,7 +35,7 @@ public class MobileWikiTestMainActivity extends
 		assertTrue("False content in TextView", !mathResult.equals(""));
 	}
 
-	/*Press Search Button and start activity*/
+	/* Press Search Button and start activity */
 	public void test_startSearchActivity() {
 		activity = getActivity();
 		assertNotNull(activity);
@@ -71,34 +70,30 @@ public class MobileWikiTestMainActivity extends
 		getInstrumentation().waitForIdleSync();
 		SearchActivity childActivity = (SearchActivity) getInstrumentation()
 				.waitForMonitorWithTimeout(activityMonitor, 5);
-		//ChildActivity is created and gain focus on screen:
+		// ChildActivity is created and gain focus on screen:
 		assertNotNull(childActivity);
 
-		final ImageButton button2 = (ImageButton) childActivity
-				.findViewById(R.id.search_button);
-
 		try {
-			Thread.sleep(100);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+			final ImageButton button2 = (ImageButton) childActivity
+					.findViewById(R.id.search_button);
 
-		childActivity.finish();
-		
-		try {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
 			getInstrumentation().waitForIdleSync();
-			assertNotNull(button2);			
-		}
-		catch (Exception e) {
+			assertNotNull(button2);
+		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			childActivity.finish();
 		}
 
 	}
 
-	/*Execute search with query that should fail*/
+	/* Execute search with query that should fail */
 	public void test_executeSearchNoResults() {
 		activity = getActivity();
 		assertNotNull(activity);
@@ -133,11 +128,67 @@ public class MobileWikiTestMainActivity extends
 		getInstrumentation().waitForIdleSync();
 		SearchActivity childActivity = (SearchActivity) getInstrumentation()
 				.waitForMonitorWithTimeout(activityMonitor, 5);
-		//ChildActivity is created and gain focus on screen:
+		// ChildActivity is created and gain focus on screen:
 		assertNotNull(childActivity);
+		try {
 
-		final ListView listView = (ListView) childActivity
-				.findViewById(R.id.listView1);
+			final ListView listView = (ListView) childActivity
+					.findViewById(R.id.listView1);
+
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			getInstrumentation().waitForIdleSync();
+			assertNotNull(listView);
+			assertEquals(listView.getCount(), 1);
+			StableArrayAdapter adapter = (StableArrayAdapter) listView
+					.getAdapter();
+			assertEquals(adapter.getItem(0),
+					activity.getResources().getString(R.string.noentries));
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			childActivity.finish();
+		}
+	}
+
+	public void test_executeSearches() {
+		executeSearch("Hallo");
+		executeSearch("HUHU");
+		executeSearch("Test");
+	}
+
+	/* Execute search with query that should fail */
+	public void executeSearch(final String query) {
+		activity = getActivity();
+		assertNotNull(activity);
+
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// Create an ActivityMonitor that monitor ChildActivity, do not
+		// interrupt, do not return mock result:
+		Instrumentation.ActivityMonitor activityMonitor = getInstrumentation()
+				.addMonitor(SearchActivity.class.getName(), null, false);
+
+		// Simulate a button click in MainActivity that start ChildActivity for
+		// result:
+		final TextView textViewQuery = (TextView) activity
+				.findViewById(R.id.search_text);
+		final ImageButton button = (ImageButton) activity
+				.findViewById(R.id.search_button);
+		activity.runOnUiThread(new Runnable() {
+			public void run() {
+				textViewQuery.setText(query);
+				button.performClick();
+			}
+		});
 
 		try {
 			Thread.sleep(100);
@@ -145,20 +196,35 @@ public class MobileWikiTestMainActivity extends
 			e.printStackTrace();
 		}
 
-		
+		getInstrumentation().waitForIdleSync();
+		SearchActivity childActivity = (SearchActivity) getInstrumentation()
+				.waitForMonitorWithTimeout(activityMonitor, 5);
+		// ChildActivity is created and gain focus on screen:
+		assertNotNull(childActivity);
 		try {
+			assertEquals(textViewQuery.getText(), query);
+			
+			final ListView listView = (ListView) childActivity
+					.findViewById(R.id.listView1);
+
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
 			getInstrumentation().waitForIdleSync();
 			assertNotNull(listView);
 			assertEquals(listView.getCount(), 1);
-			StableArrayAdapter adapter = (StableArrayAdapter)listView.getAdapter();
-			assertEquals(adapter.getItem(0), activity.getResources().getString(R.string.no_entries));
-		}
-		catch (Exception e) {
+			StableArrayAdapter adapter = (StableArrayAdapter) listView
+					.getAdapter();
+			assertEquals(adapter.getItem(0),
+					activity.getResources().getString(R.string.noentries));
+		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			childActivity.finish();
-		}		
+		}
 	}
-	
+
 }
