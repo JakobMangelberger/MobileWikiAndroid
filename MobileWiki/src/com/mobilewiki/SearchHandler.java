e93f0c8b0cc9aad85ecafc60eab0efaa07d49474
StefanTischler <stefan@omen.at>
2013-05-29 11:57:04 +0200

    [0.2.W, 01] Added options for search type and sort (Tischler, Holzegger)

--- MobileWiki/src/com/mobilewiki/SearchHandler.java
+++ MobileWiki/src/com/mobilewiki/SearchHandler.java
@@ -1,6 +1,5 @@
 package com.mobilewiki;
 
-import android.util.Log;
 import com.mobilewiki.tables.IWikiArticle;
 import com.mobilewiki.tables.IWikiContent;
 import com.mobilewiki.tables.WikiArticle;
@@ -11,32 +10,43 @@ import java.util.List;
 
 public class SearchHandler {
     private static SearchHandler _instance;
-    public static SearchHandler getInstance() {
-        if(null == _instance)
-            new SearchHandler();
-        return _instance;
-    }
 
     private SearchHandler() {
         _instance = this;
     }
 
-    public List<IWikiArticle> search_articles(String phrase) {
+    public static SearchHandler getInstance() {
+        if (null == _instance)
+            new SearchHandler();
+        return _instance;
+    }
+
+    public List<IWikiArticle> search_articles(String phrase, boolean allKeywords) {
         SQLHandler sqlHandler = SQLHandler.getInstance();
         int[] ids = sqlHandler.get_ids();
         String[] keywords = phrase.split(" ");
 
         List<IWikiArticle> articles = new ArrayList<IWikiArticle>();
-        for(int i = 0; i < ids.length; i++)
-        {
+        for (int i = 0; i < ids.length; i++) {
             IWikiArticle tempArticle = new WikiArticle(i);
             IWikiContent tempContent = new WikiContent(tempArticle.getLastContentId());
-            for(String keyword : keywords) {
-                if(tempContent.getTags().contains(keyword))
-                {
-                    articles.add(tempArticle);
-                    break;
+            if (!allKeywords) {
+                for (String keyword : keywords) {
+                    if (tempContent.getTags().contains(keyword)) {
+                        articles.add(tempArticle);
+                        break;
+                    }
                 }
+            } else {
+                boolean allKeywordsFound = true;
+                for(String keyword : keywords) {
+                    if(!tempContent.getTags().contains(keyword)) {
+                        allKeywordsFound = false;
+                        break;
+                    }
+                }
+                if(allKeywordsFound)
+                    articles.add((tempArticle));
             }
         }
 
