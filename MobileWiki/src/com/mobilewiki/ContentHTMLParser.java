284b61f701079e7527bca0190390105eb9aa9157
IrfanSehic <irfan.sehic@student.tugraz.at>
2013-06-12 11:54:18 +0200

    [0.4.W, 06][Refactoring] < Syntax Highlighting WORKS!!!!!!11 > (Sehic, Prott)

--- MobileWiki/src/com/mobilewiki/ContentHTMLParser.java
+++ MobileWiki/src/com/mobilewiki/ContentHTMLParser.java
@@ -1,16 +1,42 @@
 package com.mobilewiki;
 
-import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import android.graphics.Color;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.TextUtils;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
+import android.util.Log;
+import android.widget.EditText;
+
 public class ContentHTMLParser implements IHTMLConstants {
+	private static Matcher m_array;
+	private static Pattern p_array;
 	static ContentHTMLParser instance_;
+
+	public ContentHTMLParser() {
+
+		Log.e("length",
+				Integer.toString(IHTMLConstants.custom_tag_array.length));
+
+		p_array = Pattern.compile(IHTMLConstants.custom_tag_array[1]);
+
+		// for (int iter = 0; iter < IHTMLConstants.custom_tag_array.length;
+		// iter++) {
+		// p_array[iter] = Pattern
+		// .compile(IHTMLConstants.custom_tag_array[iter]);
+		// }
+	}
 
 	public static ContentHTMLParser getInstance() {
 		if (instance_ == null) {
 			instance_ = new ContentHTMLParser();
 		}
+
 		return instance_;
 	}
 
@@ -74,8 +100,7 @@ public class ContentHTMLParser implements IHTMLConstants {
 			if (customToHTML) {
 				destination = matcher.replaceAll(dest_tag1);
 			} else {
-				destination = matcher.replaceAll(NEWLINE + dest_tag1
-						+ DOUBLE_NEWLINE);
+				destination = matcher.replaceAll(NEWLINE + dest_tag1 + NEWLINE);
 			}
 		}
 
@@ -96,6 +121,112 @@ public class ContentHTMLParser implements IHTMLConstants {
 		// Log.e("destination", destination);
 
 		return destination;
+	}
+
+	public EditText highlightSyntax(EditText et) {
+
+		Spanned spanned_text = et.getText();
+		String string_text = et.getText().toString();
+
+		// find all occurrences forward
+		Log.e("Searchig", ":)");
+
+		int size = 100;
+		int[] index_start = new int[size];
+		int[] type_length = new int[size];
+		int i = 0;
+		int pos = 0;
+
+		for (int david = 0; david < IHTMLConstants.custom_tag_array.length; david++) {
+			while ((i = (string_text.indexOf(
+					IHTMLConstants.custom_tag_array[david], i) + 1)) > 0
+					&& pos < 100) {
+				pos++;
+				Log.e("found at", Integer.toString(i));
+				Log.e("I: ", Integer.toString(i));
+				index_start[pos] = i - 1;
+				type_length[pos] = IHTMLConstants.custom_tag_array[david]
+						.length();
+			}
+		}
+		//
+		for (int bla = 0; bla < size - 1; bla++) {
+			if (type_length[bla] == CUSTOM_BREAKLINE_TAG.length()) {
+				((Spannable) spanned_text).setSpan(new ForegroundColorSpan(
+						Color.CYAN), index_start[bla], index_start[bla]
+						+ type_length[bla], Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				((Spannable) spanned_text).setSpan(new StyleSpan(
+						android.graphics.Typeface.BOLD), index_start[bla],
+						index_start[bla] + type_length[bla],
+						Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+			} else if (type_length[bla] == CUSTOM_START_BOLD_TAG.length()
+					|| type_length[bla] == CUSTOM_END_BOLD_TAG.length()) {
+				((Spannable) spanned_text).setSpan(new ForegroundColorSpan(
+						Color.GREEN), index_start[bla], index_start[bla]
+						+ type_length[bla], Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				((Spannable) spanned_text).setSpan(new StyleSpan(
+						android.graphics.Typeface.BOLD), index_start[bla],
+						index_start[bla] + type_length[bla],
+						Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+			} else if (type_length[bla] == CUSTOM_START_PARA_TAG.length()
+					|| type_length[bla] == CUSTOM_END_PARA_TAG.length()) {
+				((Spannable) spanned_text).setSpan(new ForegroundColorSpan(
+						Color.MAGENTA), index_start[bla], index_start[bla]
+						+ type_length[bla], Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				((Spannable) spanned_text).setSpan(new StyleSpan(
+						android.graphics.Typeface.BOLD), index_start[bla],
+						index_start[bla] + type_length[bla],
+						Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+			} else if (type_length[bla] == CUSTOM_START_IMAGE_TAG.length()
+					|| type_length[bla] == CUSTOM_END_IMAGE_TAG.length()) {
+				((Spannable) spanned_text).setSpan(new ForegroundColorSpan(
+						Color.BLUE), index_start[bla], index_start[bla]
+						+ type_length[bla], Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				((Spannable) spanned_text).setSpan(new StyleSpan(
+						android.graphics.Typeface.BOLD), index_start[bla],
+						index_start[bla] + type_length[bla],
+						Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+			} else if (type_length[bla] == CUSTOM_START_TITLE_TAG.length()
+					|| type_length[bla] == CUSTOM_END_TITLE_TAG.length()) {
+				((Spannable) spanned_text).setSpan(new ForegroundColorSpan(
+						Color.RED), index_start[bla], index_start[bla]
+						+ type_length[bla], Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				((Spannable) spanned_text).setSpan(new StyleSpan(
+						android.graphics.Typeface.BOLD), index_start[bla],
+						index_start[bla] + type_length[bla],
+						Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+			}
+
+		}
+		et.setText(spanned_text);
+		/*
+		 * 
+		 * 
+		 * m_array =
+		 * Pattern.compile(IHTMLConstants.custom_tag_array[1]).matcher(
+		 * content);
+		 * 
+		 * while (m_array.find()) { Log.e("some tag", m_array.group());
+		 * SpannableStringBuilder sb = new SpannableStringBuilder(
+		 * m_array.group()); SpannableStringBuilder sb_2 = new
+		 * SpannableStringBuilder( m_array.group()); final ForegroundColorSpan
+		 * fcs = new ForegroundColorSpan(Color.rgb( 255, 100, 50)); final
+		 * ForegroundColorSpan fcs_2 = new ForegroundColorSpan( Color.rgb(255,
+		 * 100, 50));
+		 * 
+		 * // Set the text color for first 4 characters sb.setSpan(fcs, 0,
+		 * m_array.group().length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+		 * sb_2.setSpan(fcs_2, 0, m_array.group().length(),
+		 * Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+		 * 
+		 * SpannableStringBuilder sb2 = (SpannableStringBuilder) TextUtils
+		 * .concat(sb, " ", sb_2);
+		 * 
+		 * // make them also bold et.setText(sb); }
+		 */
+
+		return et;
+
 	}
 
 }
