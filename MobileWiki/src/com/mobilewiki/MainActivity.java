da4210380a23ca69e75a073135ff381564bd5b98
Claudio <Claudio@129.27.171.89>
2013-06-12 16:24:23 +0200

    [0.4.W, 1] <Startseite auslesen und falls Titel zur Main Activity übergeben wird, dann soll diese Seite angezeigt werden.> (CK, IS)

--- MobileWiki/src/com/mobilewiki/MainActivity.java
+++ MobileWiki/src/com/mobilewiki/MainActivity.java
@@ -21,6 +21,8 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private TextView article_text_view;
+	
 	private ImageGetter imgGetter = new ImageGetter() {
 		public Drawable getDrawable(String source) {
 			Drawable drawable = getResources().getDrawable(
@@ -42,27 +44,31 @@ public class MainActivity extends Activity {
 		super.onStart();
 		final EditText searchPhrase = (EditText) findViewById(R.id.search_text);
 
-		Bundle bundle = getIntent().getExtras();
 		int article_id = 1;
+		String title = "";
 		
+		Bundle bundle = getIntent().getExtras();
 		if (bundle != null) {
-			if (bundle.getString("article_id") != null) {
-				article_id = Integer.parseInt(bundle.getString("article_id").toString());
+			if (bundle.getString("title") != null) {
+				title = bundle.getString("title").toString();
 			}
 		}
 		
-		TextView article_text_view = (TextView) findViewById(R.id.article_content);
-		article_text_view.setMovementMethod(new ScrollingMovementMethod());		
-
-		RequestHandler request_handler = RequestHandler.getInstance();	
+		RequestHandler request_handler = RequestHandler.getInstance();
+		
+		if (!title.equals("")) {
+			article_id = request_handler.getArticleIdForTitle(title);
+		}
 		
 		List<Integer> content_ids = request_handler.getContentIdsforArticleId(article_id);
-		String title = request_handler.getTitleForArticleId(article_id);
-		String content = request_handler.getContentForContentId(content_ids.get(0));
+		title = request_handler.getTitleForArticleId(article_id);
+		String content = request_handler.getContentForContentId(content_ids.get(0));		
 		
 		ContentHTMLParser content_html_parser = ContentHTMLParser.getInstance();
 		String text_view = ContentHTMLParser.CUSTOM_START_TITLE_TAG + title + ContentHTMLParser.CUSTOM_END_TITLE_TAG + content;
-
+		
+		article_text_view = (TextView) findViewById(R.id.article_content);
+		article_text_view.setMovementMethod(new ScrollingMovementMethod());	
 		article_text_view.setText(Html.fromHtml(content_html_parser.parseFromCustomToHtml(text_view)));
 
 		ImageButton searchButton = (ImageButton) findViewById(R.id.search_button);
@@ -113,22 +119,24 @@ public class MainActivity extends Activity {
 		Intent intent;
 		// Handle item selection
 		switch (item.getItemId()) {
-		case R.id.edit_article:
-			intent = new Intent(MainActivity.this, EditorActivity.class);
-			Bundle parameters = new Bundle(1);
-			parameters.putString("ARTICLE_ID", "1");
-			intent.putExtras(parameters);
-			startActivity(intent);
-			return true;
-		case R.id.about:
-            intent = new Intent(MainActivity.this, AboutActivity.class);
-            startActivity(intent);
-            return true;
-		case R.id.exit_app:
-            super.finish();
-
-		default:
-			return super.onOptionsItemSelected(item);
+			case R.id.edit_article:
+				intent = new Intent(MainActivity.this, EditorActivity.class);
+				Bundle parameters = new Bundle(1);
+				parameters.putString("content", article_text_view.getText().toString());
+				intent.putExtras(parameters);
+				startActivity(intent);
+				return true;
+				
+			case R.id.about:
+	            intent = new Intent(MainActivity.this, AboutActivity.class);
+	            startActivity(intent);
+	            return true;
+	            
+			case R.id.exit_app:
+	            super.finish();
+	
+			default:
+				return super.onOptionsItemSelected(item);
 		}
 	}
 }
