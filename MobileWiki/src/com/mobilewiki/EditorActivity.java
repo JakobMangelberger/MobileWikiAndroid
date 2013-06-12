12cfec0f05558ab6683df3b41786972cc0a1ca61
IrfanSehic <irfan.sehic@student.tugraz.at>
2013-06-12 11:54:18 +0200

    [0.4.W, 06][Refactoring] < Syntax Highlighting WORKS!!!!!!11 > (Sehic, Prott)

--- MobileWiki/src/com/mobilewiki/EditorActivity.java
+++ MobileWiki/src/com/mobilewiki/EditorActivity.java
@@ -2,10 +2,12 @@ package com.mobilewiki;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.text.Html;
-import android.text.Html.ImageGetter;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
@@ -18,17 +20,42 @@ public class EditorActivity extends Activity implements IHTMLConstants {
 	private String str = "<p>Das ist ein <b>Test</b></p> ";
 	private EditText ed_view;
 
+	private static int CASE_BOLD = 1;
+	private static int CASE_PARAGRAPH = 2;
+	private static int CASE_IMAGE = 3;
+	private static int CASE_NEWLINE = 4;
+	private static int CASE_TITLE = 5;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.editor_layout); // TODO layout
 		ed_view = (EditText) findViewById(R.id.edit_text);
 
-		String cnt = ContentHTMLParser.getInstance().parseFromHtmlToCustom(title + 
-				str + "<p>Das ist ein <b>Borat!!</b></p>" + "<img src=\"borat.jpeg\"/>" + 
-		title + str + str);
-		
+		String cnt = ContentHTMLParser.getInstance().parseFromHtmlToCustom(
+				title + str + "<p>Das ist ein <b>Borat!!</b></p>"
+						+ "<img src=\"borat.jpeg\"/>" + title + str + str);
+
+		final SpannableStringBuilder sb = new SpannableStringBuilder(
+				"your text here");
+		final ForegroundColorSpan fcs = new ForegroundColorSpan(Color.rgb(255,
+				100, 50));
+		final StyleSpan bss = new StyleSpan(android.graphics.Typeface.BOLD);
+
+		// Span to make text bold
+		sb.setSpan(fcs, 0, 4, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+
+		// Set the text color for first 4 characters
+		sb.setSpan(bss, 0, 4, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+
+		// make them also bold
+		// ed_view.setText(cnt);
+
+		// Call syntax highlighter
 		ed_view.setText(cnt);
+		ed_view = ContentHTMLParser.getInstance().highlightSyntax(ed_view);
+
+		// ed_view.setText(cnt);
 	}
 
 	@Override
@@ -57,24 +84,29 @@ public class EditorActivity extends Activity implements IHTMLConstants {
 		// Handle item selection
 		switch (item.getItemId()) {
 		case R.id.add_bold:
-			addTagsToContent(2);
+			addTagsToContent(CASE_BOLD);
 			return super.onOptionsItemSelected(item);
 		case R.id.add_paragraph:
-			addTagsToContent(1);
+			addTagsToContent(CASE_PARAGRAPH);
 			return super.onOptionsItemSelected(item);
 		case R.id.add_image:
-			addTagsToContent(3);
+			addTagsToContent(CASE_IMAGE);
 			return super.onOptionsItemSelected(item);
 		case R.id.add_newline:
-			addTagsToContent(4);
+			addTagsToContent(CASE_NEWLINE);
+			return super.onOptionsItemSelected(item);
+		case R.id.add_title:
+			addTagsToContent(CASE_TITLE);
 			return super.onOptionsItemSelected(item);
 		case R.id.preview_art:
 			Intent intent = new Intent(EditorActivity.this,
 					PreviewActivity.class);
 			Bundle parameters = new Bundle(1);
 			String raw = ed_view.getText().toString();
+
 			raw = ContentHTMLParser.getInstance().parseFromCustomToHtml(raw);
 			parameters.putString("PREVIEW_TEXT", raw);
+
 			intent.putExtras(parameters);
 			Toast.makeText(this, "This is just a preview!", Toast.LENGTH_LONG)
 					.show();
@@ -101,31 +133,30 @@ public class EditorActivity extends Activity implements IHTMLConstants {
 
 		String new_string = null;
 		int temp_pos = 0;
-		switch (switch_num) {
-		case 1:
+		if (switch_num == CASE_BOLD) {
+			new_string = first_part + CUSTOM_START_BOLD_TAG
+					+ CUSTOM_END_BOLD_TAG + end_part;
+			temp_pos = first_part.length() + CUSTOM_START_BOLD_TAG.length();
+		} else if (switch_num == CASE_PARAGRAPH) {
 			new_string = first_part + CUSTOM_START_PARA_TAG
 					+ CUSTOM_END_PARA_TAG + end_part;
 			temp_pos = first_part.length() + CUSTOM_START_PARA_TAG.length();
-			break;
-		case 2:
+		} else if (switch_num == CASE_IMAGE) {
+			new_string = first_part + CUSTOM_START_IMAGE_TAG
+					+ CUSTOM_END_IMAGE_TAG + end_part;
+			temp_pos = first_part.length() + CUSTOM_START_IMAGE_TAG.length();
+		} else if (switch_num == CASE_NEWLINE) {
+			new_string = first_part + CUSTOM_BREAKLINE_TAG + end_part;
+			temp_pos = first_part.length() + CUSTOM_BREAKLINE_TAG.length();
+		} else if (switch_num == CASE_TITLE) {
 			new_string = first_part + CUSTOM_START_TITLE_TAG
 					+ CUSTOM_END_TITLE_TAG + end_part;
 			temp_pos = first_part.length() + CUSTOM_START_TITLE_TAG.length();
-			break;
-		case 3:
-			new_string = first_part + CUSTOM_START_IMAGE_TAG
-			+ CUSTOM_END_IMAGE_TAG + end_part;
-			temp_pos = first_part.length() + CUSTOM_START_IMAGE_TAG.length();
-			break;
-		case 4:
-			new_string = first_part + CUSTOM_BREAKLINE_TAG + end_part;
-			temp_pos = first_part.length() + CUSTOM_BREAKLINE_TAG.length();
-			break;
-		default:
-			break;
 		}
 
 		ed_view.setText(new_string);
+		ed_view = ContentHTMLParser.getInstance().highlightSyntax(ed_view);
+
 		final int new_pos = temp_pos;
 		ed_view.post(new Runnable() {
 			@Override
@@ -134,15 +165,24 @@ public class EditorActivity extends Activity implements IHTMLConstants {
 			}
 		});
 	}
-	
+
 	@Override
-	public void onSaveInstanceState(Bundle savedInstanceState){
+	public void onSaveInstanceState(Bundle savedInstanceState) {
 		super.onSaveInstanceState(savedInstanceState);
-		  savedInstanceState.putString("Content", ed_view.getText().toString());
+		savedInstanceState.putString("Content", ed_view.getText().toString());
+		ed_view = ContentHTMLParser.getInstance().highlightSyntax(ed_view);
 	}
-	
+
 	@Override
 	public void onRestoreInstanceState(Bundle savedInstanceState) {
+		// This overloads the syntax highlight ing
+		ed_view = ContentHTMLParser.getInstance().highlightSyntax(ed_view);
 		ed_view.setText(savedInstanceState.getString("Content"));
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		ed_view = ContentHTMLParser.getInstance().highlightSyntax(ed_view);
 	}
 }
